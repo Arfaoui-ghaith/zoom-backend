3805368fac78dc3ae4d21e3c18bcd42e8046993c
Arfaoui-ghaith <ghaith.arfaoui34@gmail.com>
2023-06-24 12:50:03 +0100

    push environment variables

--- server.js
+++ server.js
@@ -6,13 +6,10 @@ process.on('uncaughtException', (err) => {
     console.log('UNCAUGHT EXCEPTION! Shutting down....');
     process.exit(1);
 });
-
-
 const port = process.env.PORT || 9000;
 const server = app.listen(port, async() => {
     console.log(`App running on Port ${port}`);
 });
-
 process.on('unHandledRejection', (err) => {
     console.log(err.name, err.message);
     console.log('UNHANDLER REJECTION! Shutting down....');
